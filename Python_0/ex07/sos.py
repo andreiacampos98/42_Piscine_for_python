2249eb6eee26fd29e3ba3957455092c3d08d2b4f
andreiacampos98 <andreiacampos98.15@gmail.com>
2025-07-18 21:08:46 +0100

    add updates/corrections

--- Python_0/ex07/sos.py
+++ Python_0/ex07/sos.py
@@ -2,6 +2,7 @@ import sys
 
 
 def sos(s: str):
+    """Transform a string in a morse code."""
     NESTED_MORSE = {
                         " ": "/ ",
                         "A": ".- ",
@@ -59,4 +60,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
--- Python_0/ex08/Loading.py
+++ Python_0/ex08/Loading.py
@@ -1,11 +1,13 @@
 import sys
+from typing import Iterator
 
 
-def ft_tqdm(lst: range) -> None:
+def ft_tqdm(lst: range) -> Iterator[int]:
+    """My own tqdm function, Creates a load bar."""
     total = len(lst)
     for i, item in enumerate(lst, 1):
         percentage = int((i/total) * 100)
         bar = '=' * (percentage // 2) + '>' + ' ' * ((100 - percentage) // 2)
-        sys.stdout.write(f'\r{percentage}% |{bar}|{i}/{total}')
+        sys.stdout.write(f'\r{percentage}% |{bar}| {i}/{total}')
         yield item
     return
