2f048aef9f9927b55a2d87d1bd5794ee33b909bc
andreiacampos98 <andreiacampos98.15@gmail.com>
2025-07-22 21:12:31 +0100

    add python4

--- Python_4/ex00/statistics.py
+++ Python_4/ex00/statistics.py
@@ -1,16 +1,67 @@
+def ft_mean(args: tuple):
+    if not args:
+        print("ERROR")
+        return
+    return sum(args)/len(args)
+
+
+def ft_median(args: tuple):
+    if not args:
+        print("ERROR")
+        return
+    list_args = sorted(args)
+    count = len(list_args)
+    mid = count // 2
+    if count % 2 == 0:
+        return ((list_args[mid] + list_args[mid - 1]) / 2)
+    else:
+        return (list_args[mid])
+
+
+def ft_var(args: tuple):
+    if not args:
+        print("ERROR")
+        return
+    mean = ft_mean(args)
+    new_list = [(x - mean)**2 for x in args]
+    return (sum(new_list)/len(new_list))
+
+
+def ft_std(args: tuple):
+    if not args:
+        print("ERROR")
+        return
+    return (ft_var(args) ** 0.5)
+
+
+def ft_quartile(args: tuple):
+    if not args:
+        print("ERROR")
+        return
+    list_args = sorted(args)
+    count = len(list_args)
+    q_1 = count // 4
+    if q_1 % 2 == 0:
+        q_1_value = ((list_args[q_1] + list_args[q_1 - 1]) / 2)
+    else:
+        q_1_value = (list_args[q_1])
+    q_3 = (count // 4) * 3
+    if q_3 % 2 == 0:
+        q_3_value = ((list_args[q_3] + list_args[q_3 - 1]) / 2)
+    else:
+        q_3_value = (list_args[q_3])
+    return (q_1_value, q_3_value)
 
 
 def ft_statistics(*args, **kwargs) -> None:
-    print(args)
     for k, val in kwargs.items():
         if val == "mean":
-            mean(args)
-        elif val =="median":
-            median(args)
-        elif val=="std":
-            std(args)
-        elif val=="var":
-            var(args)
-        elif val=="quartile":
-            quartile(args)
-        print(k, val)
+            print(f"mean: {ft_mean(args)}")
+        elif val == "median":
+            print(f"median: {ft_median(args)}")
+        elif val == "std":
+            print(f"std: {ft_std(args)}")
+        elif val == "var":
+            print(f"var: {ft_var(args)}")
+        elif val == "quartile":
+            print(f"quartile: {ft_quartile(args)}")
